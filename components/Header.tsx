849bf8f58e6a4696ceba62c64c463a96cf62ea79
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-01-13 20:10:59 +0200

    check problem solved5

--- components/Header.tsx
+++ components/Header.tsx
@@ -41,7 +41,6 @@ export const Header: FC<HeaderProps> = async ({
       userId: {
         notIn: [...existingUserIds, profile.userId],
       },
-      // You can add additional conditions if needed
     },
   });
 
@@ -67,7 +66,7 @@ export const Header: FC<HeaderProps> = async ({
         </>
       )}
       <div className="flex items-center gap-2.5 ml-auto">
-        {whereClause === "home" && <HomeIcons initialUsers={usersWithoutConversation} />}
+        {/* {whereClause === "home" && <HomeIcons initialUsers={usersWithoutConversation} />} */}
         {whereClause === "conversation" && (
           <ConversationIcons conversationId={conversationId!} />
         )}
